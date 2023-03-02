c9fc9881c37fa1c76915aef6653c46a92a5fe34c
KKchan1119 <kkchan1119@gmail.com>
2023-03-02 09:13:25 +0900

    step1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        System.out.printf("명령) ");
+        String cmd = sc.nextLine();
+    }
+}
