ad2049fbec54c1e07c50befdc2ef4466ce8dd4b0
KKchan1119 <kkchan1119@gmail.com>
2023-03-02 09:22:35 +0900

    step 4

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int count = 0;
+
         System.out.println("== 명언 앱 ==");
+
+
         while(true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
@@ -12,6 +16,8 @@ public class Main {
                 String input = sc.nextLine();
                 System.out.printf("작가 : ");
                 String writer = sc.nextLine();
+                count ++;
+                System.out.println(count+"번 명언이 등록되었습니다.");
             }
             if(cmd.equals("종료")){
                 break;
